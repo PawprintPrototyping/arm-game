5fce6a431fd8cdc6e8a09aa56646698f7c64aef8
Rechner Fox <659028+rechner@users.noreply.github.com>
2023-06-30 22:36:46 -0700

    Woops

--- scoreboard/scoreboard.py
+++ scoreboard/scoreboard.py
@@ -1,44 +1,62 @@
 #!/usr/bin/env python
-from samplebase import SampleBase
-from rgbmatrix import graphics
+import os
+import threading
+
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
+
+from rgbbase import RGBBase
+if DEBUG:
+    from RGBMatrixEmulator import graphics
+else:
+    from rgbmatrix import graphics
+
 import time
 import random
 
-class GraphicsTest(SampleBase):
+class Scoreboard(RGBBase):
     def __init__(self, *args, **kwargs):
-        super(GraphicsTest, self).__init__(*args, **kwargs)
+        super(Scoreboard, self).__init__(*args, **kwargs)
 
-    def run(self):
+        self.minutes = kwargs.get("minutes", 1)
+        self.seconds = kwargs.get("seconds", 0)
+        self.stop = False
+        self.pause = False
+        self.delay_thread = threading.Thread()
+
+        self.black = graphics.Color(0, 0, 0)
+        self.big_font = graphics.Font()
+        self.big_font.LoadFont("fonts/texgyre-27.bdf")
+
+        self.font = graphics.Font()
+        self.font.LoadFont("fonts/10x20.bdf")
+
+        self.small_font = graphics.Font()
+        self.small_font.LoadFont("fonts/5x8.bdf")
+
+    def run(self, timer_minutes=None, timer_seconds=None):
+        if timer_minutes is None:
+            timer_minutes = self.minutes
+        if timer_seconds is None:
+            timer_seconds = self.seconds
         canvas = self.matrix.CreateFrameCanvas()
-
-        big_font = graphics.Font()
-        big_font.LoadFont("fonts/texgyre-27.bdf")
-
-        font = graphics.Font()
-        font.LoadFont("fonts/10x20.bdf")
-
-        small_font = graphics.Font()
-        small_font.LoadFont("fonts/5x8.bdf")
 
         red = graphics.Color(255, 0, 0)
         yellow = graphics.Color(255, 255, 0)
         green = graphics.Color(0, 255, 0)
         blue = graphics.Color(0, 0, 255)
         white = graphics.Color(255, 255, 255)
-        black = graphics.Color(0, 0, 0)
 
         message_colors = [yellow, green, blue, white]
         encouraging_message = self.get_encouraging_message()
         encouraging_color = random.choice(message_colors)
 
         #graphics.DrawLine(canvas, 5, 5, 22, 13, red)
-        for minutes in range(0, -1, -1):
-            for seconds in range(1, -1, -1):
+        for minutes in range(timer_minutes-1, -1, -1):
+            for seconds in range(timer_seconds-1, -1, -1):
                 if seconds % 7 == 0:
                     encouraging_message = self.get_encouraging_message()
                     encouraging_color = random.choice(message_colors)
                 for dsec in range(9, -1, -1):
-                    graphics.DrawText(canvas, font, 2, 10, blue, "Text")
                     canvas.Clear()
                     sep = ":"
                     if dsec % 2:
@@ -47,20 +65,30 @@ class GraphicsTest(SampleBase):
                     if minutes == 0 and seconds < 30:
                         color = red
 
-                    graphics.DrawText(canvas, font, 1, 25, color, f"{minutes:>2}:{seconds:>02}{'.' if sep == ':' else ' '}{dsec}")
-                    graphics.DrawText(canvas, small_font, 75, 22, encouraging_color, encouraging_message)
+                    graphics.DrawText(canvas, self.font, 1, 25, color, f"{minutes:>2}:{seconds:>02}{'.' if sep == ':' else ' '}{dsec}")
+                    graphics.DrawText(canvas, self.small_font, 75, 22, encouraging_color, encouraging_message)
 
                     canvas = self.matrix.SwapOnVSync(canvas)
                     time.sleep(0.1)
+                    if self.stop:
+                        return
+                    while self.pause:
+                        time.sleep(0.1)
 
         #graphics.DrawCircle(canvas, 15, 15, 10, green)
 
+        time.sleep(10)   # show display for 10 seconds before exit
+
+    def game_over(self, blocking=False):
+        canvas = self.matrix.CreateFrameCanvas()
         canvas.Clear()
         canvas.Fill(255, 0, 0)
-        graphics.DrawText(canvas, big_font, 12, 25, black, "GAME OVER")
-        canvas = self.matrix.SwapOnVSync(canvas)
+        graphics.DrawText(canvas, self.big_font, 12, 25, self.black, "GAME OVER")
+        self.matrix.SwapOnVSync(canvas)
+        if blocking:
+            while not self.stop:
+                time.sleep(0.1)
 
-        time.sleep(10)   # show display for 10 seconds before exit
 
     def get_encouraging_message(self):
         potential_messages = [
@@ -70,12 +98,11 @@ class GraphicsTest(SampleBase):
           "[Encouraging Message 4]",
           "Sharp shootin', Tex"
         ]
-
         return random.choice(potential_messages)
         
 
 # Main function
 if __name__ == "__main__":
-    graphics_test = GraphicsTest()
+    graphics_test = Scoreboard()
     if (not graphics_test.process()):
         graphics_test.print_help()
