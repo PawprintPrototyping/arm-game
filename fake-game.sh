b6e49f0e12e5cc4f895d086919bff07d66066612
Rechner <rechner@pawprintprototyping.org>
2023-07-09 22:28:16 -0700

    fake gamegit add fake-game.sh

--- fake-game.sh
+++ fake-game.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+mosquitto_pub -h arm-display -t /scoreboard/digits/clear -m ''
 mosquitto_pub -h arm-display -t /scoreboard/rgb/start_timer -m ''
 mosquitto_pub -h arm-display -t /motion/motion/start -m ''
 mosquitto_pub -h arm-display -t /target_movement/start -m ''
+mosquitto_pub -h arm-display -t /targets/3/enable -m ''
 
 mosquitto_sub -h arm-display -t /scoreboard/timer/game_over -C 1
+
+mosquitto_pub -h arm-display -t /targets/3/disable -m ''
+mosquitto_pub -h arm-display -t /scoreboard/rgb/clear -m ""
+mosquitto_pub -h arm-display -t /scoreboard/rgb/game_over -m "GAME OVER"
 mosquitto_pub -h arm-display -t /motion/motion/stop -m ''
 mosquitto_pub -h arm-display -t /target_movement/stop -m ''
