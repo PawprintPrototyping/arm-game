4f158a02f9ece0e420cdfce77d304c0e49dbad58
Rechner Fox <659028+rechner@users.noreply.github.com>
2023-07-11 20:27:55 -0700

    Cheating

---
+++ mqtt_dingding.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#set -x
+
+PIN='4'
+ON_DURATION='0.02'
+DWELL_DURATION='0.25'
+MQTT_HOST="arm-display"
+
+while [ 1 ]; do
+  mosquitto_sub -h $MQTT_HOST \
+    -C 1 \
+    -t /scoreboard/bell \
+    -t /scoreboard/rgb/start_timer \
+    -t /scoreboard/timer/game_over \
+
+  if ! [ -e "/sys/class/gpio/gpio${PIN}/value" ] ; then
+    echo "$PIN" > '/sys/class/gpio/export'
+  fi
+  if [[ "$(cat "/sys/class/gpio/gpio${PIN}/direction")" != "out" ]] ; then
+    echo 'out' > "/sys/class/gpio/gpio${PIN}/direction"
+  fi
+
+  echo '1' > "/sys/class/gpio/gpio${PIN}/value"
+  sleep "$ON_DURATION"
+  echo '0' > "/sys/class/gpio/gpio${PIN}/value"
+  sleep "$DWELL_DURATION"
+  echo '1' > "/sys/class/gpio/gpio${PIN}/value"
+  sleep "$ON_DURATION"
+  echo '0' > "/sys/class/gpio/gpio${PIN}/value"
+
+  sleep 0.5
+done
